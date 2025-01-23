b4dbf730e28e52f5d9c26d8856088b7ed2e59ee5
Christopher <chris.m.hahn@web.de>
2025-01-23 17:40:49 +0100

    current state of login/register/auth

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
 import {QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -10,13 +9,9 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            {/* <AuthProvider> */}
-                <QueryClientProvider client={queryClient}>
-                    <App />
-                    <ReactQueryDevtools />
-                </QueryClientProvider>
-            {/* </AuthProvider> */}
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools />
+        </QueryClientProvider>
     </StrictMode>,
 )
