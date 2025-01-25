fe9058c8a18881423f5b58988bf88bd2f39f2ae1
Claire Wollbrett <uhnmh@student.kit.edu>
2025-01-25 21:41:57 +0100

    test

--- src/components/SinglePaper.tsx
+++ src/components/SinglePaper.tsx
@@ -1,6 +1,15 @@
 
 import {useQuery} from "@tanstack/react-query";
 import Box from "@mui/material/Box";
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
 
 interface PaperElement {
     id: number;
@@ -15,6 +24,8 @@ interface PaperElement {
     reviewLimit: number;
 //    requests: SinglePaperRequestListEntry
 }
+
+
 
 export default function SinglePaper() {
     const {
@@ -38,20 +49,62 @@ export default function SinglePaper() {
         return <span>{`Error!: ${paperError.message}`}</span>;
     }
 
+  
+
     let paperObject: PaperElement = paperData[1];
     return (
         <>
+            <Paper sx={{ width: '100%' }}>
             <h2>{paperObject.title}</h2>
-            <h2>{paperObject.authors}</h2>
-            <div>{`Active ${paperObject.active}`}</div>
-            <div>{`Internal: ${paperObject.internal}`}</div>
-            <div>{`Reviews: X/${paperObject.reviewLimit}`}</div>
+            <h3>{`Authors : ${paperObject.authors}`}</h3>
+            <Button
+              component="label"
+              role={undefined}
+              variant="contained"
+              tabIndex={-1}
+              startIcon={<FileDownloadIcon />}
+            >
+              Download paper
+            </Button>
+            {/*<div>{`Active :  ${paperObject.active}`}</div>
+            <div>{`Internal : ${paperObject.internal}`}</div>*/}
+            <Stack spacing={1} sx={{ alignItems: 'center' }}>
+              <Chip label={paperObject.internal === true ? 'Internal' : 'External'}/>
+            </Stack>
+            <div>{`Reviews : X/${paperObject.reviewLimit}`}</div>
             <Box sx={{ display: 'grid', gridTemplateRows: 'repeat(3, 1fr)' }}>
                 <div>{`Preview: ${paperObject.abstractText}`}</div>
             </Box>
+            <List
+              sx={{
+              width: '100%',
+              maxWidth: 600,
+              bgcolor: 'background.paper',
+              position: 'relative',
+              overflow: 'auto',
+              maxHeight: 250,
+              '& ul': { padding: 0 },
+              }}
+              subheader={<li />}
+            >
+              {[0, 1, 2, 3, 4].map((sectionId) => (
+                <li key={`section-${sectionId}`}>
+                  <ul>
+                    <ListSubheader>{`review status ${sectionId}`}</ListSubheader>
+                    {[0, 1, 2].map((item) => (
+                      <ListItem key={`item-${sectionId}-${item}`}>
+                        <ListItemText primary={`Review ${item}`} />
+                      </ListItem>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </List>
             <Box>
                 <div>{`Authors note: ${paperObject.authorsNote}`}</div>
             </Box>
+            </Paper>
+            
             <div>{`Upload Date: ${paperObject.uploadDate}`}</div>
 
             {/*
