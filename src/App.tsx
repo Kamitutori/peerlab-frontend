897da4304dddf9881518872ea7c3129f7ca256fc
Dennis Tu <dyaojun.tu@gmail.com>
2025-01-28 16:01:30 +0100

    fixes after merge

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,20 @@ import ForgotPassword from "./pages/ForgotPassword";
 import SinglePaper from "./components/SinglePaper.tsx";
 
 const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#90caf9',
+        },
+        secondary: {
+            main: '#f48fb1',
+        },
+        background: {
+            default: '#121212',
+            paper: '#1d1d1d',
+        },
+
+    },
     components: {
         MuiTextField: {
             defaultProps: {
