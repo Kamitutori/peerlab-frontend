0248d4abf705f4ce6245068769a75a153fb367be
Christopher <chris.m.hahn@web.de>
2025-01-30 17:50:38 +0100

    regex problems

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -12,7 +12,7 @@ function goToRegister() {
 }
 
 export default function LoginPage() {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const emailRegex = /^(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])$/;
     const {login} = useUpdateAuth();
     const [showPassword, setShowPassword] = useState(false);
 
@@ -46,9 +46,10 @@ export default function LoginPage() {
             alert("Please fill in all fields.");
             return;
         }
-        /* if (emailRegex.test(email)) {
+         /* if (emailRegex.test(email)) {
             alert("Your email has no valid format.")
-        }*/
+             return;
+        } */
         await login({email, password});
         // TODO Statements here are not reached if successful. Remove alerts and insert error message here. Problem: how to transfer the response code?
     };
