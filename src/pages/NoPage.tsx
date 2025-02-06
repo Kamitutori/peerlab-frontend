d46ebf708edcea0ad01a789e2d2f4e8694441b3e
Claire Wollbrett <uhnmh@student.kit.edu>
2025-02-06 22:19:27 +0100

    comments/NoPage

--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,9 +1,12 @@
 import Paper from "@mui/material/Paper";
 
+// This is the default export of the NoPage component which will be displayed for a 404 error.
 export default function NoPage() {
     return (
         <div>
+            {/* Using the Paper component from Material-UI */}
             <Paper sx={{ padding: 4 }}>
+                {/* Displaying the error message for 404: Not Found */}
                 <h1>Error 404: Not Found</h1>
             </Paper>
         </div>
