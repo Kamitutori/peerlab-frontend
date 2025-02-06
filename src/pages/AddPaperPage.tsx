6b3f79a20ca97dc75a2183c4ad9f2b0bd8f8d5c6
Claire Wollbrett <uhnmh@student.kit.edu>
2025-02-06 21:52:35 +0100

    comments/AddPaperPage

--- src/pages/AddPaperPage.tsx
+++ src/pages/AddPaperPage.tsx
@@ -1,7 +1,9 @@
 import PaperPageForm from '../components/PaperPageForm.tsx';
 
+// Importing the PaperPageForm component, which is a form to add and edit a paper.
 export default function AddPaperPage() {
     return (
+        // Returning the PaperPageForm component as part of the UI of the AddPaperPage component.
         <PaperPageForm
         />
     );
